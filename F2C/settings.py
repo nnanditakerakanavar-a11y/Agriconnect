9a003809a8e0434f1ceaa15da062688cca257103
nnanditakerakanavar-a11y <nnanditakerakanavar@gmail.com>
2026-04-13 12:19:07 +0530

    updated allowed hosts

--- F2C/settings.py
+++ F2C/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-$x$h0h_xk2$a9jqu-i^_9iqxlmbxa*qbstl1px1yyybww4x%=+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://farmpoject.onrender.com', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['farmpoject.onrender.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
